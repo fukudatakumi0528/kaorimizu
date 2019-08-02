049991e01508ba5e2ac55638fb967e7168818822
OkumuraYousuke <okumura_yousuke@giginc.co.jp>
2019-08-02 17:37:29 +0900

    front:3000 一旦up

--- node/src/js/utils/slick.js
+++ node/src/js/utils/slick.js
@@ -1,5 +1,47 @@
 export default class Slick {
 	slickInit() {
+
+		const $topSlickSelecter = $('.js-slickSlider--topkv');
+		const $topSlickItem = $('.js-slickSlider--topkv li');
+
+		//
+		// top KV スライダー
+		//
+
+
+		$topSlickSelecter.slick({
+			autoplay: true,
+			autoplaySpeed: 5000,
+			fade: true,
+			speed: 2000,
+			arrows: false,
+			dots: true,
+			appendDots: $('.kv__slider_dots'),
+/*
+			responsive: [
+				{
+					breakpoint: 480,
+					settings: {
+						//dots: true,
+						arrows: true,
+						slidesToShow: 1,
+						centerMode: true,
+						centerPadding: '40px',
+						initialSlide: 1,
+					}
+				}
+			]
+*/
+		});
+		$(window).on('load resize', function(){
+			let $containerH = $topSlickSelecter.innerHeight();
+			$topSlickItem.innerHeight($containerH);
+		});
+
+
+
+
+
 		$('.js-slickSlider--pickup').slick({
 			dots: true,
 			slidesToShow: 3,
