87d9a3c94fa2ed4218a75c6acaeff4a0fd74323d
Fukuda Takumi <fukuda_takumi@giginc.co.jp>
2019-10-03 17:59:03 +0900

    add 諸々追加

--- node/src/js/utils/fixedButton.js
+++ node/src/js/utils/fixedButton.js
@@ -5,6 +5,33 @@ export default class FixedButton {
     const $innerTopperInnerMenuLine = $('.l-fixed-button__inner__topper__inner__menu__line');
     const $innerMainButton = $('.l-fixed-button__inner__main__button');
 
+    //menu部分
+    const $spMenu = $('#js-spMenu')
+
+    //スクロール禁止にするための設定
+    function handleTouchMove(event) {
+      event.preventDefault();
+    }
+
+    //スクロール禁止
+    function scrollStop() {
+      document.addEventListener('touchmove', handleTouchMove, { passive: false });
+    };
+
+    //スクロール復帰
+    function scrollStart() {
+      document.removeEventListener('touchmove', handleTouchMove, { passive: false });
+    };
+
+    //スクロールの解除、非解除
+    function scrollControl() {
+      if ($spMenu.hasClass('is-active')) {
+        scrollStop();
+      } else {
+        scrollStart();
+      };
+    }
+
     $innerTopperInner.on('click', function () {
       $innerMainButton.removeClass('is-click');
       $innerTopperInnerMenuLine.toggleClass('is-active');
@@ -13,12 +40,19 @@ export default class FixedButton {
       } else {
         $innerTopperInnerText.html('menu');
       }
+
+      $spMenu.toggleClass('is-active');
+      scrollControl();
     });
 
     $innerMainButton.on('click', function () {
       $innerMainButton.not(this).removeClass('is-click');
       $innerTopperInnerMenuLine.removeClass('is-active');
       $(this).toggleClass('is-click');
+
+      //searchボタンがクリックされた時は、必ずspMenuは閉じている。
+      $spMenu.removeClass('is-active');
+      scrollControl();
     });
 	};
 };
