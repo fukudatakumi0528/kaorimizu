144f7008a30dcf2adf852d9ef4bf28a966ac120a
Fukuda Takumi <fukuda_takumi@giginc.co.jp>
2019-10-04 14:50:59 +0900

    add SPメニュー作成完了

--- node/src/js/utils/fixedButton.js
+++ node/src/js/utils/fixedButton.js
@@ -4,9 +4,14 @@ export default class FixedButton {
     const $innerTopperInnerText = $('.l-fixed-button__inner__topper__inner__text');
     const $innerTopperInnerMenuLine = $('.l-fixed-button__inner__topper__inner__menu__line');
     const $innerMainButton = $('.l-fixed-button__inner__main__button');
+    const $spSearchTrigger = $('#js-spSearch-trigger');
+    const $spGrantTrigger = $('#js-spGrant-trigger');
 
     //menu部分
-    const $spMenu = $('#js-spMenu')
+    const $spMenu = $('#js-spMenu');
+    const $spSearch = $('#js-spSearch');
+    const $spGrant = $('#js-spGrant');
+
 
     //スクロール禁止にするための設定
     function handleTouchMove(event) {
@@ -32,6 +37,47 @@ export default class FixedButton {
       };
     }
 
+    //spMenuの出し入れ
+    function spMenuToggle() {
+      if (!$spMenu.hasClass('is-active')) {
+        $spMenu.addClass('is-active');
+      } else {
+        $spMenu.removeClass('is-active');
+        $spMenu.addClass('is-left');
+        setTimeout(function () {
+          $spMenu.removeClass('is-left');
+        },400)
+      }
+    };
+
+    //spSearchの出し入れ
+    function spSearchToggle() {
+      if (!$spSearch.hasClass('is-active')) {
+        $spSearch.addClass('is-active');
+      } else {
+        $spSearch.removeClass('is-active');
+        $spSearch.addClass('is-left');
+        setTimeout(function () {
+          $spSearch.removeClass('is-left');
+        },400)
+      }
+    };
+
+    //spGrantの出し入れ
+    function spGrantToggle() {
+      if (!$spGrant.hasClass('is-active')) {
+        $spGrant.addClass('is-active');
+      } else {
+        $spGrant.removeClass('is-active');
+        $spGrant.addClass('is-left');
+        setTimeout(function () {
+          $spGrant.removeClass('is-left');
+        },400)
+      }
+    };
+
+
+    //spMenu開閉用ボタンを押した時
     $innerTopperInner.on('click', function () {
       $innerMainButton.removeClass('is-click');
       $innerTopperInnerMenuLine.toggleClass('is-active');
@@ -40,19 +86,99 @@ export default class FixedButton {
       } else {
         $innerTopperInnerText.html('menu');
       }
+      spMenuToggle();
 
-      $spMenu.toggleClass('is-active');
+      //grantボタンがクリックされた時は、必ずspMenuは閉じている。
+      if ($spSearch.hasClass('is-active')) {
+        $spSearch.removeClass('is-active');
+        $spSearch.addClass('is-right');
+        setTimeout(function () {
+          $spSearch.removeClass('is-right');
+        },400)
+      };
+
+      //searchボタンがクリックされた時は、必ずspGrantは閉じている。
+      if ($spGrant.hasClass('is-active')) {
+        $spGrant.removeClass('is-active');
+        $spGrant.addClass('is-right');
+        setTimeout(function () {
+          $spGrant.removeClass('is-right');
+        },400)
+      };
+
       scrollControl();
     });
 
-    $innerMainButton.on('click', function () {
+
+    
+    //spSearch開閉用ボタンを押した時
+    $spSearchTrigger.on('click', function () {
       $innerMainButton.not(this).removeClass('is-click');
       $innerTopperInnerMenuLine.removeClass('is-active');
+      if ($innerTopperInnerMenuLine.hasClass('is-active')) {
+        $innerTopperInnerText.html('close');
+      } else {
+        $innerTopperInnerText.html('menu');
+      }
       $(this).toggleClass('is-click');
 
+      spSearchToggle();
+
       //searchボタンがクリックされた時は、必ずspMenuは閉じている。
-      $spMenu.removeClass('is-active');
-      scrollControl();
+      if ($spMenu.hasClass('is-active')) {
+        $spMenu.removeClass('is-active');
+        $spMenu.addClass('is-right');
+        setTimeout(function () {
+          $spMenu.removeClass('is-right');
+        },400)
+      };
+      
+      //searchボタンがクリックされた時は、必ずspGrantは閉じている。
+      if ($spGrant.hasClass('is-active')) {
+        $spGrant.removeClass('is-active');
+        $spGrant.addClass('is-right');
+        setTimeout(function () {
+          $spGrant.removeClass('is-right');
+        },400)
+      };
+
+      scrollStart();
     });
+
+
+    //spGrant開閉用ボタンを押した時
+    $spGrantTrigger.on('click', function () {
+      $innerMainButton.not(this).removeClass('is-click');
+      $innerTopperInnerMenuLine.removeClass('is-active');
+      if ($innerTopperInnerMenuLine.hasClass('is-active')) {
+        $innerTopperInnerText.html('close');
+      } else {
+        $innerTopperInnerText.html('menu');
+      }
+      $(this).toggleClass('is-click');
+
+      spGrantToggle();
+
+      //grantボタンがクリックされた時は、必ずspMenuは閉じている。
+      if ($spMenu.hasClass('is-active')) {
+        $spMenu.removeClass('is-active');
+        $spMenu.addClass('is-right');
+        setTimeout(function () {
+          $spMenu.removeClass('is-right');
+        },400)
+      };
+
+      //grantボタンがクリックされた時は、必ずspMenuは閉じている。
+      if ($spSearch.hasClass('is-active')) {
+        $spSearch.removeClass('is-active');
+        $spSearch.addClass('is-right');
+        setTimeout(function () {
+          $spSearch.removeClass('is-right');
+        },400)
+      };
+      
+      scrollStart();
+    });
+    
 	};
 };
