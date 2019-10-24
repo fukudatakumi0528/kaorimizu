cf7d26b8d71fc4fd6254c2b6356c2d7fe86d91cc
Fukuda Takumi <fukuda_takumi@giginc.co.jp>
2019-10-24 18:45:42 +0900

    修正

--- app/wp-content/themes/finbee_media/home.php
+++ app/wp-content/themes/finbee_media/home.php
@@ -657,6 +657,42 @@
 	<?php endif;?>
 
 	<?php 
+    $taxonomies = array( 
+      'feature_tag',
+      'hobby_tag',
+      'life_tag',
+      'learn_tag',
+    );
+  
+    $args = array(
+      'orderby'       => 'name', 
+      'order'         => 'ASC',
+      'hide_empty'    => true, 
+      'exclude'       => array(), 
+      'exclude_tree'  => array(), 
+      'include'       => array(),
+      'number'        => '', 
+      'fields'        => 'all', 
+      'slug'          => '', 
+      'parent'        => '',
+      'hierarchical'  => true, 
+      'child_of'      => 0, 
+      'childless'     => false,
+      'get'           => '', 
+      'name__like'    => '',
+      'description__like' => '',
+      'pad_counts'    => false, 
+      'offset'        => '', 
+      'search'        => '', 
+      'cache_domain'  => 'core'
+    ); 					
+
+    $popularTags = get_terms($taxonomies, $args);
+
+    usort($popularTags,"sort_count");
+
+    $rankingPopularTags = array_slice($popularTags,0,10);
+
 		if(count($rankingPopularTags) > 0):
 	?>
 	<section class="p-top__keyword">
