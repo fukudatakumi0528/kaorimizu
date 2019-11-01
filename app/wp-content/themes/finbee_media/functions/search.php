7961719d63cd726599cdc38129587dde447fc0ed
Fukuda Takumi <fukuda_takumi@giginc.co.jp>
2019-11-01 18:22:53 +0900

    検索範囲をタイトルのみに限定

--- app/wp-content/themes/finbee_media/functions/search.php
+++ app/wp-content/themes/finbee_media/functions/search.php
@@ -3,20 +3,35 @@
  * サイト内検索の範囲に、カテゴリー名、タグ名、を含める
 ************************************************/
 function custom_search($search, $wp_query) {
-  global $wpdb;
-  
-  //サーチページ以外だったら終了
-  if (!$wp_query->is_search)
-      return $search;
-  
-  if (!isset($wp_query->query_vars))
-      return $search;
-  
-  if(is_search()) {
-    // カスタム投稿タイプを検索結果に含める
-    $search .= "AND (post_type = 'feature' OR post_type='hobby' OR post_type='life' OR post_type='learn')";
-  }
+    global $wpdb;
+    
+    //サーチページ以外だったら終了
+    if (!$wp_query->is_search)
+        return $search;
+    
+    if (!isset($wp_query->query_vars))
+        return $search;
+    
+    $q = $wp_query->query_vars;
 
+    $n = ! empty( $q['exact'] ) ? '' : '%';
+    $search =
+    $searchand = '';
+
+    foreach ( (array) $q['search_terms'] as $term ) {
+        $term = esc_sql( like_escape( $term ) );
+        $search .= "{$searchand}($wpdb->posts.post_title LIKE '{$n}{$term}{$n}')";
+        $searchand = ' AND ';
+    }
+
+    if ( ! empty( $search ) ) {
+        $search = " AND ({$search}) AND (post_type = 'feature' OR post_type='hobby' OR post_type='life' OR post_type='learn')";
+
+        if ( ! is_user_logged_in() ) {
+            $search .= " AND ($wpdb->posts.post_password = '')  ";
+        }
+    }
+    
   /*
   ユーザー名とか、タグ名・カテゴリ名も検索対象にする場合
   $search_words = explode(' ', isset($wp_query->query_vars['s']) ? $wp_query->query_vars['s'] : '');
