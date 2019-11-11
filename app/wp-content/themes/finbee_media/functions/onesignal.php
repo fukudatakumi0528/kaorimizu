6b8d25c8e156b4a43509c3ad8ceee90b82cecefa
Fukuda Takumi <fukuda_takumi@giginc.co.jp>
2019-11-11 10:11:36 +0900

    onesignalテスト（本文に記事タイトルを挿入できるかどうかのテスト）

---
+++ app/wp-content/themes/finbee_media/functions/onesignal.php
@@ -0,0 +1,31 @@
+<?php
+
+function onesignal_send_notification_filter($fields, $new_status, $old_status, $post)
+{
+    if (defined('ENV') && ENV == 'prod') {
+      if($new_status == 'publish'){
+        if($post->post_type == 'feature' || $post->post_type == 'hobby' || $post->post_type == 'learn' || $post->post_type == 'life'){
+          switch($old_status){
+              case 'draft':
+              case 'pending':
+              case 'auto-draft':
+              case 'future':
+              case 'new':
+                  // アクティブユーザーのみに通知する
+                  $fields = array(
+                      'app_id' => "fa6c61c4-e8f0-4c0a-ad34-89ef3f2c2175",
+                      'included_segments' => array('Active Users'),
+                      'headings' => array("en" => "be-topia(ビートピア)"),
+                      'isAnyWeb' => true,
+                      'isAndroid' => true,
+                      'isIos' => true,
+                      'url' => get_permalink($post->ID),
+                      'contents' => array("en" => "「" .get_the_title($post->ID)) ."」が公開されました！"
+                  );
+          }
+        }
+      }
+    }
+    return $fields;
+}
+add_filter('onesignal_send_notification', 'onesignal_send_notification_filter', 10, 4);
