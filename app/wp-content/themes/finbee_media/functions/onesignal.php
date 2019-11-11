8bc3fba3465f1171502e85bb227ed3552c2b9f13
Fukuda Takumi <fukuda_takumi@giginc.co.jp>
2019-11-11 11:06:06 +0900

    修正

--- app/wp-content/themes/finbee_media/functions/onesignal.php
+++ app/wp-content/themes/finbee_media/functions/onesignal.php
@@ -2,30 +2,27 @@
 
 function onesignal_send_notification_filter($fields, $new_status, $old_status, $post)
 {
-  $contents = "「" .get_the_title($post->ID) ."」が公開されました！";
-    if (defined('ENV') && ENV == 'prod') {
-      if($new_status == 'publish'){
-        if($post->post_type == 'feature' || $post->post_type == 'hobby' || $post->post_type == 'learn' || $post->post_type == 'life'){
-          switch($old_status){
-              case 'draft':
-              case 'pending':
-              case 'auto-draft':
-              case 'future':
-              case 'new':
-                  // アクティブユーザーのみに通知する
-                  $fields = array(
-                      'app_id' => "fa6c61c4-e8f0-4c0a-ad34-89ef3f2c2175",
-                      'included_segments' => array('Active Users'),
-                      'headings' => array("en" => "be-topia(ビートピア)", "ja" => "be-topia(ビートピア)"),
-                      'isAnyWeb' => true,
-                      'isAndroid' => true,
-                      'isIos' => true,
-                      'contents' => array("en" => $contents, "ja" => $contents),
-                  );
-          }
-        }
+  if($new_status == 'publish'){
+    if($post->post_type == 'feature' || $post->post_type == 'hobby' || $post->post_type == 'learn' || $post->post_type == 'life'){
+      switch($old_status){
+          case 'draft':
+          case 'pending':
+          case 'auto-draft':
+          case 'future':
+          case 'new':
+              $contents = "「" .get_the_title($post->ID) ."」が公開されました！";
+              // アクティブユーザーのみに通知する
+              $fields = array(
+                  'app_id' => "fa6c61c4-e8f0-4c0a-ad34-89ef3f2c2175",
+                  'included_segments' => array('Active Users'),
+                  'headings' => array("en" => "be-topia(ビートピア)", "ja" => "be-topia(ビートピア)"),
+                  'isAnyWeb' => true,
+                  'url' => get_permalink($post->ID),
+                  'contents' => array("en" => $contents, "ja" => $contents),
+              );
       }
     }
-    return $fields;
+  }
+  return $fields;
 }
 add_filter('onesignal_send_notification', 'onesignal_send_notification_filter', 10, 4);
