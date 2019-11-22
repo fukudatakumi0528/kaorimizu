9cdd1d7f8bc25433c5814f773e84ea27376c56b6
Fukuda Takumi <fukuda_takumi@giginc.co.jp>
2019-11-22 14:58:17 +0900

    カスタム投稿フィールドの修正

--- app/wp-content/themes/finbee_media/functions/customfield.php
+++ app/wp-content/themes/finbee_media/functions/customfield.php
@@ -1,23 +1,5 @@
 <?php
   if( function_exists('acf_add_options_page') ) {
-    // メインビジュアルスライダー
-    acf_add_options_page(array(
-      'page_title'     => 'トップスライダー',
-      'menu_title'    => 'トップスライダー',
-      'menu_slug'     => 'top-slider',
-      'capability'    => 'edit_posts',
-      'redirect'        => true,
-      'icon_url'    => 'dashicons-tag'
-    ));
-    // トップページ一覧（特集、趣味等）
-    acf_add_options_page(array(
-      'page_title'     => 'トップページ一覧',
-      'menu_title'    => 'トップページ一覧',
-      'menu_slug'     => 'top-popular',
-      'capability'    => 'edit_posts',
-      'redirect'        => true,
-      'icon_url'    => 'dashicons-tag'
-    ));
     // 全カテゴリ（PICK UP）
     acf_add_options_page(array(
       'page_title'     => '全カテゴリ（PICK UP）',
