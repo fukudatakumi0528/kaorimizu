3691ee47d3f2c7b50b0908c17cf14f6a9183e36d
Fukuda Takumi <fukuda_takumi@giginc.co.jp>
2019-11-26 11:01:15 +0900

    アフィリエイトボタン部分の外部リンク化

--- node/src/js/utils/affiliate.js
+++ node/src/js/utils/affiliate.js
@@ -2,9 +2,16 @@ export default class Affiliate {
   linkCustom() {
     $(setTimeout(function () {
 
+      const $btnNames = $('.easyLink-info-name').children('a');
       const $btnAmazons = $('.easyLink-info-btn-amazon');
       const $btnRakutens = $('.easyLink-info-btn-rakuten');
       const $btnYahoos = $('.easyLink-info-btn-yahoo');
+
+      if ($btnNames) {
+        $btnNames.each(function (i, elem) {
+          elem.setAttribute('target', '_brank');
+        })
+      }      
   
       if ($btnAmazons) {
         $btnAmazons.each(function (i, elem) {
